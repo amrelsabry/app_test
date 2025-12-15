944b8ef068eae4e2f9e11109b305d3c4e1305a31
Amro1 <amr.amro228@gmail.com>
2025-12-15 15:20:37 +0300

    create app test dashboard

---
+++ lib/views/home.dart
@@ -0,0 +1,53 @@
+import 'package:app_test/widget/card_info.dart';
+import 'package:app_test/widget/charts_card.dart';
+import 'package:app_test/widget/header_text.dart';
+import 'package:app_test/widget/side_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          // sideBar
+          SideBar(),
+
+          // content page
+          Expanded(
+            child: Column(
+              children: [
+                // HeaderText
+                HeaderText(),
+                const SizedBox(height: 40,),
+                // card info
+                Wrap(
+                  spacing: 10,
+                  runAlignment: WrapAlignment.spaceBetween,
+                  children: [
+                      CardInfo(),
+                      CardInfo(),
+                      CardInfo(),
+                      CardInfo(),
+                  ],
+                ),
+          
+                // content page
+                Wrap(
+                  spacing: 16,
+                  runSpacing: 16,
+                  children: [
+                  ChartsCard(),
+
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widget/card_info.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CardInfo extends StatelessWidget {
+  const CardInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: SizedBox(
+                    height: 100,
+                    width: 250,
+                    child: Card(
+                      elevation: 4,
+                      color: Colors.blueGrey.shade500,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.message, size: 38,color: Colors.white,),
+                            const SizedBox(width: 13,),
+                            Text("5",style: TextStyle(fontSize: 38 , color:  Colors.white),),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+  }
+}
